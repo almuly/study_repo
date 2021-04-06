15f01ad770a98cdbc53ef4ec31bfbc8f0f1ed084
Alex <muly_1993@mail.ru>
2021-04-06 15:19:48 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 140,
+    },
+});
+
+
+export default function App() {
+
+    const classes = useStyles();
+    const [weather, setWeather] = useState({});
+    const [city, setCity] = useState("london");
+
+
+    useEffect(() => {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0ccf3aee26b06d4238093a1863d04d55`)
+            .then(response => response.json())
+            .then(json => setWeather(json))
+
+    }, [city])
+
+
+    return (
+        <div className="app">
+
+                    <input
+                        type="text"
+                        value={city}
+                        onChange={(e) => {
+                            setCity(e.target.value)
+                        }}
+                        placeholder="Enter location"
+                        className="location_input"
+                    />
+
+            <pre>{JSON.stringify(weather, null,2)}</pre>
+        </div>
+    );
 }
-
-export default App;

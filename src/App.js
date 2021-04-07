87245f6189e89a2b4ae31ac3f3d02dbad5e81e22
Alexander Mulyarchik <alexander.mulyarchik@solbeg.com>
2021-04-07 21:43:11 +0300

    add: grid wrapper component

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
-import Input from '@material-ui/core/input';
+import SearchIcon from '@material-ui/icons/Search';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import {TextField} from "@material-ui/core";
+
 
 const useStyles = makeStyles({
     root: {
@@ -26,16 +30,18 @@ export default function App() {
     const [weather, setWeather] = useState(null);
     const [city, setCity] = useState('minsk');
     const [value, setValue] = useState('');
+
     const handleClick = () => {
         setCity(value)
         setValue('')
     }
 
     useEffect(() => {
-        setTimeout(() => {
+        setTimeout(() => {(
+
             fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0ccf3aee26b06d4238093a1863d04d55&units=metric`)
                 .then(response => response.json())
-                .then(json => setWeather(json))
+                .then(json => setWeather(json)))
         }, 3000)
 
     }, [city])
@@ -44,50 +50,53 @@ export default function App() {
         return <p>Loading data...</p>;
     }
     return (
-        <div className="app">
-            <Card className={classes.root}>
-                <CardActionArea>
-                    <CardMedia
-                        style={{
-                            maxWidth: 300,
-                            maxHeight: '100%',
-                            margin: '0 auto'
-                        }}
-                        className={classes.media}
-                        image={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-                        title="Contemplative Reptile"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            {weather.name},{weather.sys.country}.
-                        </Typography>
-                        <Typography variant="h6" component="h3">
-                            The temperature is {weather.main.temp}°C
-                        </Typography>
-                        <Typography variant="h6" color="textSecondary" component="h3">
-                            The weather is {weather.weather[0].main}. And it is {weather.weather[0].description}
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <OutlinedInput
-                            type="text"
-                            value={value}
-                            onChange={(e) => setValue(e.target.value)}
-                            placeholder="Enter location"
-                            className="location_input"
-                            size="large"
-                            variant="filled"
+        <Container maxWidth="sm">
+            <div className="app">
+                <Card className={classes.root}>
+                    <CardActionArea>
+                        <CardMedia
+                            style={{
+                                maxWidth: 300,
+                                maxHeight: '100%',
+                                margin: '0 auto'
+                            }}
+                            className={classes.media}
+                            image={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                            title="Contemplative Reptile"
                         />
-                        <Button onClick={handleClick} color='primary' variant='contained'>
-                            Search
-                        </Button>
-                    </CardActions>
-                </CardActionArea>
-
-            </Card>
-
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                {weather.name},{weather.sys.country}.
+                            </Typography>
+                            <Typography variant="h6" component="h3">
+                                The temperature is {weather.main.temp}°C
+                            </Typography>
+                            <Typography variant="h6" color="textSecondary" component="h3">
+                                The weather is {weather.weather[0].main}. And it is a {weather.weather[0].description}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <TextField
+                                type="text"
+                                value={value}
+                                onChange={(e) => setValue(e.target.value)}
+                                variant="filled"
+                                label="Enter location"
+                            />
+                            <Button
+                                startIcon={<SearchIcon/>}
+                                onClick={handleClick}
+                                color='primary'
+                                variant='contained'>
+                                Search
+                            </Button>
+                        </CardActions>
+                    </CardActionArea>
+                </Card>
+            </div>
 
             <pre>{JSON.stringify(weather, null, 2)}</pre>
-        </div>
+        </Container>
+
     );
 }

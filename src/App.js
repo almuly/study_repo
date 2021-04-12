58613af950e368f41ae368c181c4a1d495cc1dc6
Alexander.Mulyarchik <muly_1993@mail.ru>
2021-04-12 23:13:53 +0300

    add: try to add history button(need fixes)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useReducer} from "react";
 import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -7,14 +7,13 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import SearchIcon from '@material-ui/icons/Search';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
 import {TextField} from "@material-ui/core";
 import axios from 'axios'
-import useErrorBoundary from "use-error-boundary"
 import {useSnackbar} from 'notistack';
+import History from "./History";
+
 
 const useStyles = makeStyles({
     root: {
@@ -25,54 +24,38 @@ const useStyles = makeStyles({
     },
 });
 
+export const HistoryContext = React.createContext()
+const initialState = [];
+
+function reducer(state, action) {
+    switch (action) {
+        case 'ADD':
+            return [...state, state.push(weather)];
+        default:
+            return state;
+    }
+}
+
 
 export default function App() {
-
+    const [history, dispatch] = useReducer(reducer, initialState)
     const classes = useStyles();
     const [weather, setWeather] = useState(null);
     const [city, setCity] = useState('minsk');
     const [value, setValue] = useState('');
     const {enqueueSnackbar, closeSnackbar} = useSnackbar();
-
     const handleClick = () => {
         setCity(value)
         setValue('')
     }
+
+
     const handleKeyPress = (event) => {
-        if (event.key == 'Enter') {
+        if (event.key ===
+            'Enter') {
             handleClick()
         }
     }
-
-    // useEffect(() => {
-    //     setTimeout(() => {(
-    //
-    //         fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0ccf3aee26b06d4238093a1863d04d55&units=metric`)
-    //             .then(response => response.json())
-    //             .then(json => setWeather(json)))
-    //     }, 3000)
-    //
-    // }, [city])
-
-    // useEffect(() => {
-    //     const fetchData =  () => {
-    //         const result =  axios().get( `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0ccf3aee26b06d4238093a1863d04d55&units=metric`);
-    //
-    //         if (weather !== null) {
-    //             enqueueSnackbar('Everything is OK');
-    //         } else{
-    //             enqueueSnackbar('Something wrong');
-    //         }
-    //         setWeather(result.data);
-    //
-    //
-    //     };
-    //
-    //     fetchData();
-    //
-    // }, [city])
-
-
     useEffect(() => {
         const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0ccf3aee26b06d4238093a1863d04d55&units=metric`;
         axios.get(apiUrl)
@@ -82,59 +65,67 @@ export default function App() {
             })
             .then(() => enqueueSnackbar('Everything is OK.'))
             .catch(() => enqueueSnackbar('Something go wrong'))
+
     }, [city]);
     if (weather === null) {
         return <p>Loading data...</p>;
     }
     return (
-        <Container maxWidth="sm">
-            <div className="app">
-                <Card className={classes.root}>
-                    <CardActionArea>
-                        <CardMedia
-                            style={{
-                                maxWidth: 300,
-                                maxHeight: '100%',
-                                margin: '0 auto'
-                            }}
-                            className={classes.media}
-                            image={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-                            title="Contemplative Reptile"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                {weather.name},{weather.sys.country}.
-                            </Typography>
-                            <Typography variant="h6" component="h3">
-                                The temperature is {weather.main.temp}°C
-                            </Typography>
-                            <Typography variant="h6" color="textSecondary" component="h3">
-                                The weather is {weather.weather[0].main}. And it is a {weather.weather[0].description}
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <TextField
-                                type="text"
-                                value={value}
-                                onChange={(e) => (setValue(e.target.value))}
-                                variant="filled"
-                                label="Enter location"
-                                onKeyPress={handleKeyPress}
+        <HistoryContext.Provider value={{
+            historyHistory: history
+            , historyDispatch: dispatch
+        }}>
+
+            <Container maxWidth="sm">
+                <div className="app">
+                    <Card className={classes.root}>
+                        <CardActionArea>
+                            <CardMedia
+                                style={{
+                                    maxWidth: 300,
+                                    maxHeight: '100%',
+                                    margin: '0 auto'
+                                }}
+                                className={classes.media}
+                                image={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                                title="Contemplative Reptile"
                             />
-                            <Button
-                                startIcon={<SearchIcon/>}
-                                onClick={handleClick}
-                                color='primary'
-                                variant='contained'>
-                                Search
-                            </Button>
-                        </CardActions>
-                    </CardActionArea>
-                </Card>
-            </div>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    {weather.name},{weather.sys.country}.
+                                </Typography>
+                                <Typography variant="h6" component="h3">
+                                    The temperature is {weather.main.temp}°C
+                                </Typography>
+                                <Typography variant="h6" color="textSecondary" component="h3">
+                                    The weather is {weather.weather[0].main}. And it is
+                                    a {weather.weather[0].description}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <TextField
+                                    type="text"
+                                    value={value}
+                                    onChange={(e) => (setValue(e.target.value))}
+                                    variant="filled"
+                                    label="Enter location"
+                                    onKeyPress={handleKeyPress}
+                                />
+                                <Button
+                                    startIcon={<SearchIcon/>}
+                                    onClick={handleClick}
+                                    color='primary'
+                                    variant='contained'>
+                                    Search
+                                </Button>
+                            </CardActions>
+                        </CardActionArea>
+                    </Card>
+                    <History/>
 
-            <pre>{JSON.stringify(weather, null, 2)}</pre>
-        </Container>
-
+                </div>
+                <pre>{JSON.stringify(weather, null, 2)}</pre>
+            </Container>
+        </HistoryContext.Provider>
     );
 }

7a6bfc11f50a807f864336670e01b5483d11dd5c
Alex <muly_1993@mail.ru>
2021-04-13 14:04:21 +0300

    add:favorite button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, {useState, useEffect, useReducer} from "react";
+import React, {useState, useEffect} from "react";
 import {makeStyles} from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import {
+    TextField,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Card,
+    Button,
+    Typography,
+    Container
+} from "@material-ui/core";
 import SearchIcon from '@material-ui/icons/Search';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import {TextField} from "@material-ui/core";
 import axios from 'axios'
 import {useSnackbar} from 'notistack';
-import History from "./History";
 
 
 const useStyles = makeStyles({
@@ -24,37 +25,37 @@ const useStyles = makeStyles({
     },
 });
 
-export const HistoryContext = React.createContext()
-const initialState = [];
-
-function reducer(state, action) {
-    switch (action) {
-        case 'ADD':
-            return [...state, state.push(weather)];
-        default:
-            return state;
-    }
-}
-
 
 export default function App() {
-    const [history, dispatch] = useReducer(reducer, initialState)
     const classes = useStyles();
     const [weather, setWeather] = useState(null);
     const [city, setCity] = useState('minsk');
+    const [favourite, setFavourite] = useState({name:weather.name,
+        temp:weather.main.temp,
+        date:Date.now,
+    })
     const [value, setValue] = useState('');
     const {enqueueSnackbar, closeSnackbar} = useSnackbar();
+
     const handleClick = () => {
         setCity(value)
         setValue('')
     }
-
 
     const handleKeyPress = (event) => {
         if (event.key ===
             'Enter') {
             handleClick()
         }
+    }
+    const addToFavourite = () => {
+        setFavourite(
+            [...favourite,
+                {name:weather.name,
+                    temp:weather.main.temp,
+                    date:Date.now,
+            }]
+        )
     }
     useEffect(() => {
         const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0ccf3aee26b06d4238093a1863d04d55&units=metric`;
@@ -67,65 +68,66 @@ export default function App() {
             .catch(() => enqueueSnackbar('Something go wrong'))
 
     }, [city]);
+
     if (weather === null) {
         return <p>Loading data...</p>;
     }
+
     return (
-        <HistoryContext.Provider value={{
-            historyHistory: history
-            , historyDispatch: dispatch
-        }}>
 
-            <Container maxWidth="sm">
-                <div className="app">
-                    <Card className={classes.root}>
-                        <CardActionArea>
-                            <CardMedia
-                                style={{
-                                    maxWidth: 300,
-                                    maxHeight: '100%',
-                                    margin: '0 auto'
-                                }}
-                                className={classes.media}
-                                image={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-                                title="Contemplative Reptile"
+        <Container maxWidth="sm">
+            <div className="app">
+                <Card className={classes.root}>
+                    <CardActionArea>
+                        <CardMedia
+                            style={{
+                                maxWidth: 300,
+                                maxHeight: '100%',
+                                margin: '0 auto'
+                            }}
+                            className={classes.media}
+                            image={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                            title="Contemplative Reptile"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                {weather.name},{weather.sys.country}.
+                            </Typography>
+                            <Typography variant="h6" component="h3">
+                                The temperature is {weather.main.temp}°C
+                            </Typography>
+                            <Typography variant="h6" color="textSecondary" component="h3">
+                                The weather is {weather.weather[0].main}. And it is
+                                a {weather.weather[0].description}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <TextField
+                                type="text"
+                                value={value}
+                                onChange={(e) => (setValue(e.target.value))}
+                                variant="filled"
+                                label="Enter location"
+                                onKeyPress={handleKeyPress}
                             />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="h2">
-                                    {weather.name},{weather.sys.country}.
-                                </Typography>
-                                <Typography variant="h6" component="h3">
-                                    The temperature is {weather.main.temp}°C
-                                </Typography>
-                                <Typography variant="h6" color="textSecondary" component="h3">
-                                    The weather is {weather.weather[0].main}. And it is
-                                    a {weather.weather[0].description}
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <TextField
-                                    type="text"
-                                    value={value}
-                                    onChange={(e) => (setValue(e.target.value))}
-                                    variant="filled"
-                                    label="Enter location"
-                                    onKeyPress={handleKeyPress}
-                                />
-                                <Button
-                                    startIcon={<SearchIcon/>}
-                                    onClick={handleClick}
-                                    color='primary'
-                                    variant='contained'>
-                                    Search
-                                </Button>
-                            </CardActions>
-                        </CardActionArea>
-                    </Card>
-                    <History/>
+                            <Button
+                                startIcon={<SearchIcon />}
+                                onClick={handleClick}
+                                color='primary'
+                                variant='contained'>
+                                Search
+                            </Button>
+                            <Button onClick={addToFavourite}
+                                    color='primary'>Add To Favourite</Button>
+                        </CardActions>
+                    </CardActionArea>
+                </Card>
 
-                </div>
-                <pre>{JSON.stringify(weather, null, 2)}</pre>
-            </Container>
-        </HistoryContext.Provider>
+
+            </div>
+            <pre>{JSON.stringify(weather, null, 2)}</pre>
+            <pre>{JSON.stringify(favourite, null, 2)}</pre>
+        </Container>
+
     );
 }

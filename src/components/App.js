f89fbf663883d7079032c5b1d3a2fbf013c93f02
Alex <muly_1993@mail.ru>
2021-04-20 09:27:45 +0300

    add:log in form using Formik

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,22 @@ import {Container} from "@material-ui/core";
 import HistoryList from "./History/HistoryList";
 import {useHistoryWeather} from "./History/HistoryContext";
 import WeatherCard from "./WeatherCard";
+import Modal from "./Modal";
+import useModal from "../hooks/useModal";
 
 
 export default function App() {
     const {state} = useHistoryWeather()
+    const {isShowing, toggle} = useModal();
     return (
 
         <Container maxWidth="sm">
+
+            <button className="button-default" onClick={toggle}>LogIn</button>
+            <Modal
+                isShowing={isShowing}
+                hide={toggle}
+            />
             <WeatherCard/>
             <HistoryList history={state}/>
         </Container>
